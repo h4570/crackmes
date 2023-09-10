db27853fd122e8b311efd827aace7178b5a3b23c
h4570 <sandro.sobczynski@gmail.com>
2023-09-10 02:11:10 +0200

    Add first crackme

---
+++ 1 - NBS/Solution/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿using System.Windows.Forms;
+
+namespace Keygen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.KeyTextBox = new Keygen.LogInNormalTextBox();
+            this.NameTextBox = new Keygen.LogInNormalTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Image = global::Keygen.Properties.Resources.bg;
+            this.pictureBox1.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.BackColor = System.Drawing.Color.Transparent;
+            this.GenerateButton.BackgroundImage = global::Keygen.Properties.Resources.bg;
+            this.GenerateButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.GenerateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GenerateButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GenerateButton.Location = new System.Drawing.Point(33, 287);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(295, 35);
+            this.GenerateButton.TabIndex = 5;
+            this.GenerateButton.Text = "GENERATE";
+            this.GenerateButton.UseVisualStyleBackColor = false;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // KeyTextBox
+            // 
+            this.KeyTextBox.BackColor = System.Drawing.Color.Transparent;
+            this.KeyTextBox.BackgroundColour = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.KeyTextBox.BorderColour = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.KeyTextBox.Location = new System.Drawing.Point(33, 328);
+            this.KeyTextBox.MaxLength = 32767;
+            this.KeyTextBox.Multiline = false;
+            this.KeyTextBox.Name = "KeyTextBox";
+            this.KeyTextBox.ReadOnly = true;
+            this.KeyTextBox.Size = new System.Drawing.Size(295, 29);
+            this.KeyTextBox.Style = Keygen.LogInNormalTextBox.Styles.NotRounded;
+            this.KeyTextBox.TabIndex = 4;
+            this.KeyTextBox.Text = "-- WAITING --";
+            this.KeyTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.KeyTextBox.TextColour = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.KeyTextBox.UseSystemPasswordChar = false;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.BackColor = System.Drawing.Color.Transparent;
+            this.NameTextBox.BackgroundColour = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.NameTextBox.BorderColour = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.NameTextBox.Location = new System.Drawing.Point(33, 199);
+            this.NameTextBox.MaxLength = 32767;
+            this.NameTextBox.Multiline = false;
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.ReadOnly = false;
+            this.NameTextBox.Size = new System.Drawing.Size(295, 29);
+            this.NameTextBox.Style = Keygen.LogInNormalTextBox.Styles.NotRounded;
+            this.NameTextBox.TabIndex = 2;
+            this.NameTextBox.Text = "Jack Kowalski";
+            this.NameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.NameTextBox.TextColour = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.NameTextBox.UseSystemPasswordChar = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(162, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 478);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.KeyTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Crackme 1 - Keygen";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_Closed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private LogInNormalTextBox NameTextBox;
+        private LogInNormalTextBox KeyTextBox;
+        private Button GenerateButton;
+        private Label label1;
+    }
+}
+
---
+++ 1 - NBS/Solution/Form1.cs
@@ -0,0 +1,119 @@
+﻿using Keygen.Properties;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Keygen
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _musicFilePath = Path.Combine(Path.GetTempPath(), "dreamsound.mp3");
+        private readonly WindowsMediaPlayer _player = new WindowsMediaPlayer();
+        private readonly Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(_musicFilePath))
+                {
+                    File.WriteAllBytes(_musicFilePath, Resources.dreamsound);
+                }
+
+                _player.URL = _musicFilePath;
+                _player.settings.volume = 6;
+                _player.controls.play();
+            }
+            catch { }
+        }
+
+        private void Form1_Closing(object sender, EventArgs e)
+        {
+            try
+            {
+                _player.controls.stop();
+                KeyTextBox.Text = "Disposing audio...";
+            }
+            catch { }
+        }
+
+        private void Form1_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+                var task = Task.Run(() =>
+                {
+                    if (File.Exists(_musicFilePath))
+                    {
+                        File.Delete(_musicFilePath);
+                    }
+                });
+
+                task.Wait(TimeSpan.FromMilliseconds(333));
+            }
+            catch { }
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            if (NameTextBox.Text.Length < 3)
+            {
+                KeyTextBox.Text = "Name should have at least 3 characters!";
+                return;
+            }
+
+            var key = new char[28];
+            for (int i = 0; i < 28; i++)
+            {
+                key[i] = RandomChar();
+            }
+
+            var begin = _random.Next(2, 13);
+            var beginStr = begin.ToString().PadLeft(2, '0');
+
+            key[0] = beginStr[0];
+            key[1] = beginStr[1];
+
+            var secondLetter = ((int)NameTextBox.Text[0]).ToString("X").PadLeft(2, '0');
+            var thirdLetter = ((int)NameTextBox.Text[1]).ToString("X").PadLeft(2, '0');
+            var lastLetter = ((int)NameTextBox.Text[NameTextBox.Text.Length - 1]).ToString("X").PadLeft(2, '0');
+
+            key[begin] = secondLetter[0];
+            key[begin + 1] = secondLetter[1];
+
+            key[begin * 2] = thirdLetter[0];
+            key[begin * 2 + 1] = thirdLetter[1];
+
+            key[27] = lastLetter[1];
+            key[26] = lastLetter[0];
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                if (i > 0 && i % 4 == 0)
+                {
+                    sb.Append('-');
+                }
+                sb.Append(key[i]);
+            }
+
+            KeyTextBox.Text = sb.ToString();
+        }
+
+        private char RandomChar()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return chars[_random.Next(chars.Length)];
+        }
+    }
+}
