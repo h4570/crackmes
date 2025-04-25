4e451b5610bf83e78cefc6dc2dbe2974bcb91fe7
h4570 <sandro.sobczynski@gmail.com>
2025-04-25 23:57:49 +0200

    wip - commit to be checked

---
+++ 5 - D-Montez moi/Solution/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Keygen.Properties;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Keygen
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _musicFilePath = Path.Combine(Path.GetTempPath(), "dreamsound.mp3");
+        private readonly WindowsMediaPlayer _player = new WindowsMediaPlayer();
+        private readonly Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(_musicFilePath))
+                {
+                    File.WriteAllBytes(_musicFilePath, Resources.dreamsound);
+                }
+
+                _player.URL = _musicFilePath;
+                _player.settings.volume = 6;
+                _player.controls.play();
+            }
+            catch { }
+        }
+
+        private void Form1_Closing(object sender, EventArgs e)
+        {
+            try
+            {
+                _player.controls.stop();
+                KeyTextBox.Text = "Disposing audio...";
+            }
+            catch { }
+        }
+
+        private void Form1_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+                var task = Task.Run(() =>
+                {
+                    if (File.Exists(_musicFilePath))
+                    {
+                        File.Delete(_musicFilePath);
+                    }
+                });
+
+                task.Wait(TimeSpan.FromMilliseconds(333));
+            }
+            catch { }
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            if (NameTextBox.Text.Length < 1)
+            {
+                KeyTextBox.Text = "Name should have at least 1 character!";
+                return;
+            }
+
+            KeyTextBox.Text = NameTextBox.Text.Length.ToString();
+
+            int key2 = 0;
+
+            foreach (var letter in NameTextBox.Text)
+            {
+                key2 += letter * 1337;
+            }
+
+            Key2TextBox.Text = key2.ToString();
+            int key3 = 0;
+
+            foreach (var letter in NameTextBox.Text)
+            {
+                key3 += letter;
+            }
+
+            Key3TextBox.Text = key3.ToString();
+            ButtonTextBox.Text = "Button -> X:0, Y:1";
+        }
+
+        private char RandomChar()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return chars[_random.Next(chars.Length)];
+        }
+    }
+}
---
+++ 5 - D-Montez moi/TODOREADME.md
@@ -0,0 +1,46 @@
+# CrackMe #2 by falcon1
+
+<div align="center">
+  <img src="proof.png" height="400px" width="auto">
+</div>
+
+## Summary 
+Difficulty: Easy  
+Packed: No  
+Anti-debug: Yes  
+
+---
+
+## Description
+
+```
+Hi,
+This is my first crackme...
+It is compiled with masm32.
+Don't expect it to any hard(or advanced) at all...
+I couldn't decide which packing method to use (UPX or AsPack).
+In the end, I chose not to pack it at all...
+Here's the scheme:
+Using your reversing knowledge you have to
+1)Find out which one of the 84(or 54 in hexadecimal) buttons is the right
+one that invokes the serial checking routine
+2)Write a valid keygenerator
+3)There's no three...
+It is recommended (for your own practice) that you don't patch the app.
+Oh, and one more thing, there is an other way to beat the crackme...
+(Using your reversing knowledge again),find out how this can be done...
+(or if you are curious and can't figure it out on your own, read 'hints.txt').
+
+That's all..
+Good Luck
+
+Falcon1
+http://george-v.servegame.com
+
+
+Remarks:A big thank you goes to all the members of BiW who are always
+there at their forums(www.reversing.be) ready to help you in your reversing quest...
+
+
+Created At:29 July 2005
+```
